2e5b7994c43ee523046d9b5139d61c9f7aa9ebf3
Dplo1514 <dladlsgur3334@gmail.com>
2022-02-06 02:44:49 +0900

    220206 update

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask , render_template ,jsonify ,request
+import pymongo 
+import certifi
+
+#데이터베이스 import
+ca = certifi.where()
+client = pymongo.MongoClient('mongodb+srv://plo:1514@cluster0.eakep.mongodb.net/Cluster0?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.Hangu99
+
 app = Flask(__name__)
-
-
+#templates html 반환 부분
 @app.route('/')
 def home():
    return render_template("index.html")
@@ -10,19 +17,46 @@ def home():
 def border():
     return render_template("border.html")
  
+@app.route("/border_write")
+def border_write():
+    return render_template("border_write.html")
  
-@app.route('/test', methods=['GET'])
-def test_get():
-    title_receive = request.args.get('title_give')
-    print(title_receive)
-    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
-
-@app.route('/test', methods=['POST'])
-def test_post():
-    title_receive = request.form['title_give']
-    print(title_receive)
-    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
-
+@app.route("/login_page")
+def login_page():
+    return render_template("login.html")
+ 
+@app.route("/sing-up")
+def sign_up():
+    return render_template("sign_up.html")
+ 
+ #회원가입 기능 구현
+@app.route("/save_member", methods=["POST"])
+def save_member():
+    name_receive = request.form['name_give']
+    id_receive = request.form['id_give']
+    b_receive = request.form['b_give']
+    
+    doc = {
+      'Name': name_receive,
+      'ID':id_receive,
+      'B': b_receive
+    }
+    
+    db.users.insert_one(doc)
+    return render_template("index.html") , jsonify({'msg': '회원가입이 완료되었습니다!'})
+ 
+ #로그인 기능 구현
+@app.route("/login",methods=["POST" , "GET"])
+def login():
+    user_list = list(db.users.find({},{'_id':False}))
+    user_id = user_list[0]["ID"]
+    user_b = user_list[0]["B"]
+    user = {user_id : user_b}
+    id = request.form['id_give']
+    b = request.form['b_give']
+    doc = {id : b}
+    if user["user_id" : "user_b"] == doc[id : b]:
+       return render_template("index.html") , jsonify({'user': user_list})       
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=8000,debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,48 @@
+function save_member() {
+    let name = $('#name_input').val();
+    let id = $('#id_input').val();
+    let b = $('#b_input').val();
 
-function hey() {
-    $.ajax({
-        type: "GET",
-        url: "/test?title_give=봄날은간다",
-        data: {},
-        success: function (response) {
-            alert(response)
+    if ($('#name_input').val() == '') {
+        alert('이름을 입력해주세요.')
+        return;
+        }else if ($('#id_input').val() ==''){
+            alert('ID를 입력해주세요')
+            return;
+        }else if ($('#b_input').val() ==''){
+            alert('비밀번호를 입력해주세요')
+            return;
         }
-    })
+
+    $.ajax({
+        type: 'POST',
+        url: '/save_member',
+        data: {name_give : name ,id_give : id , b_give : b},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload;
+        }
+    });
 }
 
-function hey2() {
+function login() {
+    let id = $('#login_id').val();
+    let b = $('#login_b').val();
+
+    if ($('#login_id').val() ==''){
+        alert('ID를 입력해주세요')
+        return;
+    }else if ($('#login_b').val() ==''){
+        alert('비밀번호를 입력해주세요')
+        return;
+    }
+
     $.ajax({
-        type: "POST",
-        url: "/test",
-        data: {
-            title_give: '봄날은간다'
-        },
+        type: 'POST',
+        url: '/login',
+        data: {id_give : id , b_give : b},
         success: function (response) {
-            alert(response)
+            alert("회원가입 완료!")
         }
-    })
+    });
 }
